d61f9e67daa6cb3d43e6a8dd6ce0bb9034cca47b
Carlos P Montoya <cpmontoya@verizon.net>
2014-06-08 08:18:22 -0400

    added additional comment to plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,8 @@
 ## this script creates a histogram Global Active Power from the file 
 ## "household_power_consumption.txt". The file comes from data at 
 ## UC Irvine Machine Learning Repository   http://archive.ics.uci.edu/ml/
+## the link to download the file used below is 
+## https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 ## The histogram is created as a 480 by 480 png file "plot1.png" 
 
 ## read data from local file into data.frame
